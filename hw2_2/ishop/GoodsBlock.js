3aaab81bda0db8285ca68779cae9e2f8115beed3
Olga Zhylko <7744627@gmail.com>
2022-05-02 12:28:35 +0300

    HW 2 (part 1 Filter, part 2 ishop)

---
+++ hw2_2/ishop/GoodsBlock.js
@@ -0,0 +1,80 @@
+﻿const GoodsBlock = React.createClass({
+    displayName: 'GoodsBlock',
+    propTypes: {
+        shopName: React.PropTypes.string.isRequired,
+        features: React.PropTypes.arrayOf(
+            React.PropTypes.string.isRequired,
+        ),
+        goods: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                imgUrl: React.PropTypes.string.isRequired,
+                code: React.PropTypes.number.isRequired,
+                ironMateria: React.PropTypes.string.isRequired,
+                ironName: React.PropTypes.string.isRequired,
+                count: React.PropTypes.number.isRequired,
+            })
+        )
+    },
+    getInitialState: function () {
+        return {
+            selectedNowCode: null,
+            goods: this.props.goods,
+        };
+    },
+    deleteGood: function (code) {
+        this.setState({
+            selectedNowCode: code,
+        });
+        let indexForDelete = null;
+        this.state.goods.map((v, index) => {
+            indexForDelete = v['code'] == code ? index : indexForDelete
+        })
+        this.setState(() => {
+            {
+                goods: this.state.goods.splice(indexForDelete, 1)
+            }
+        })
+    },
+    selectGood: function (code) {
+        // console.log('выбрана строчка товара с кодом ' + code);
+        this.setState({
+            selectedNowCode: code,
+        });
+    },
+    render: function () {
+        let featuresForGoods = [];
+        for (let a = 0; a < this.props.features.length; a++) {
+            let feature = this.props.features[a];
+            let featureName = React.DOM.td({key: a, className: 'Feaucher'}, feature,); //без key React ругается,
+            featuresForGoods.push(featureName);
+        }
+        let goodsList = this.props.goods.map(v =>
+            React.createElement(GoodOne, {
+                key: v.code,
+                imgUrl: v.imgUrl,
+                code: v.code,
+                ironName: v.ironName,
+                ironMateria: v.ironMateria,
+                count: v.count,
+                cbDeleteGood: this.deleteGood,
+                cbSelectGood: this.selectGood,
+                goodSelected: this.state.goodSelected,
+                isSelected: (this.state.selectedNowCode == v.code),
+            })
+        );
+        return React.DOM.div({className: 'GoodsBlock'},
+            React.DOM.h1({className: 'ShopName'}, this.props.shopName),
+            React.DOM.table({className: 'TableGoods'},
+                React.DOM.thead({},
+                    React.DOM.tr({key: 'titlesOfFeatures', className: 'TableHeader'},
+                        featuresForGoods,
+                    ),
+                ),
+                React.DOM.tbody({},
+                    goodsList,
+                ),
+            ),
+        )
+    },
+
+});
