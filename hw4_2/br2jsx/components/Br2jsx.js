cc4530f41fdc509c0bb13a90a1c893c0aa9b9fab
Olga Zhylko <7744627@gmail.com>
2022-05-20 11:27:08 +0300

    hw4_2

---
+++ hw4_2/br2jsx/components/Br2jsx.js
@@ -0,0 +1,44 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+require('./Br2jsx.css');
+
+class Br2jsx extends React.Component {
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+    };
+
+    state = {
+        text: this.props.text,
+    }
+
+    componentWillMount() {
+        console.log(111, this.state.text);
+        let a = /<[^<>]+>/g;
+        this.setState({
+            text: this.state.text.split(a)
+            // text: this.state.text.split(a).join("<br>") //?????????
+
+        });
+    }
+
+    render() {
+        // console.log(122, this.state.text);
+        // Сейчас в return массив из слов, нет тегов.
+        // Непонятно к чему тут стремиться: к строке с тегами? К массиву с тегами
+        //  Может, вообще всё не в ту сторону?
+
+        return (<div className={"main"}>
+                {this.state.text}
+            </div>
+        )
+
+        // return (<div
+//         dangerouslySetInnerHTML = {{
+//             __html: this.state.text.join("<br>")
+//         }}/>);
+    }
+}
+
+export default Br2jsx;
+
